76f7c9ece53b6f89ddbb6eec5c7351ccb7c5738e
Liviu Felix <milorad.liviu.felix@gmail.com>
2021-11-13 15:08:30 +0100

    better working collapser with hover target

--- client/components/container.interact.collapse/main.js
+++ client/components/container.interact.collapse/main.js
@@ -2,6 +2,7 @@ class ContainerCollapser {
     #appId = 'container.interact.collapse'
 	#container = null;
     target = null;
+    #hoverTarget = null;
 
     constructor(container) {
         this.#container = container
@@ -12,26 +13,44 @@ class ContainerCollapser {
         document.addEventListener('container.edit.pos.selected', e => this.target = e.detail.id);
 		document.addEventListener('container.edit.pos.unselected', (e) => this.target = null);
         document.addEventListener("keydown", (e) => this.handleKeydown(e))
+        document.addEventListener('mouseover',(e) => this.#hoverTarget = e.target)
     }  
     
     disable() {
         document.removeEventListener('container.edit.pos.selected', e => this.target = e.detail.id);
 		document.removeEventListener('container.edit.pos.unselected', (e) => this.target = null);
-        document.addEventListener("keydown", (e) => this.handleKeydown(e))
+        document.removeEventListener('mouseover',(e) => this.#hoverTarget = e.target)
+    }
+
+    findClosestDiv(start) {
+        if(!start) {
+            return null
+        }
+
+        let node = start
+        while(node) {
+            if (node.nodeName.toLowerCase() == 'div') {
+                return node
+            }
+            node = node.parentNode
+        }
+        return node
     }
 
     collapse() {
-        if (this.target) {
-            console.log(`Collapsing ${this.target}`)
-            this.#container.setCollapseMode(this.target, {width:"32px",height:"32px"})
-            this.#container.collapse(this.target, "app:"+this.#appId)
+        let target = this.findClosestDiv(this.target || this.#hoverTarget)
+        if (target) {
+            console.log(`Collapsing ${target}`)
+            this.#container.setCollapseMode(target, {width:"32px",height:"32px"})
+            this.#container.collapse(target, "app:"+this.#appId)
         }
     }
 
     expand() {
-        if (this.target) {
-            console.log(`Expanding ${this.target}`)
-            this.#container.expand(this.target, "app:"+this.#appId)
+        let target = this.findClosestDiv(this.target || this.#hoverTarget)
+        if (target) {
+            console.log(`Expanding ${target}`)
+            this.#container.expand(target, "app:"+this.#appId)
         }
     }
 
