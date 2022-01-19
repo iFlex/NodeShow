834e25dd321cc88053af775c67ad925c462a392c
miloradf <miloradf@spotify.com>
2022-01-19 03:49:20 +0100

    added first version skeleton

---
+++ client/components/draw/main.js
@@ -0,0 +1,38 @@
+export class Draw {
+	appId = "draw"
+	#container = null;
+	#enabled = false
+	
+	constructor (container) {
+		this.#container = container;
+		container.registerComponent(this);	
+	}
+
+	enable () {
+		if (!this.#enabled) {
+			this.#enabled = true
+		}
+	}
+
+	disable () {
+		if (this.#enabled) {
+			this.#enabled = false
+		}
+	}
+
+	isEnabled () {
+		return this.#enabled
+	}
+
+	//entrypoint to container function
+	start(targets) {
+		//use this if app needs mutual exclusivity from other apps that have start & stop.
+		//reactive apps won't be affected
+		//background apps won't be affected
+		this.#container.componentStartedWork(this.appId, {})
+	}
+
+	stop() {
+		this.#container.componentStoppedWork(this.appId)
+	}
+}
